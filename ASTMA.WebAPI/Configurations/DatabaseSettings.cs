bba219bc2c47bfe976502e028279b9b6550bcf06
zikpuppy <jarkmarvis@gmail.com>
2023-07-03 16:20:37 -0400

    Db config settings class

---
+++ ASTMA.WebAPI/Configurations/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ASTMA.WebUI.Configurations;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string TaskerCollectionName { get; set; } = null!;
+}
